403d76b0808caa4fe5c20410f8a83e2176ecbf85
Dylan Petzer <dylan.petzer@entelect.co.za>
2023-05-08 10:48:51 +0200

    changed dialog boxes, made ui fixes

--- src/components/dialogs/FailedLoginDialog.jsx
+++ src/components/dialogs/FailedLoginDialog.jsx
@@ -17,7 +17,7 @@ function FailedLoginDialog({open, handleClose, message}) {
         message={message}
       >
         <DialogTitle className="items-center bg-black-200 text-secondary border-b-2 border-secondary">
-          {`Something Went Wrong, please try again!`}
+          {`Something Went Wrong!`}
         </DialogTitle>
         <DialogContent className="bg-black-200 text-secondary border-b-2 border-secondary">
          <div className="flex flex-col justify-around items-center text-secondary py-10 sm:gap-2 md:gap-10 ">
@@ -25,8 +25,8 @@ function FailedLoginDialog({open, handleClose, message}) {
             <p>{`${message}`}</p>
          </div>
         </DialogContent>
-        <DialogActions className="bg-primary flex justify-center items-center m-auto w-[100%]">
-          <Button onClick={handleClose} variant="outlined" autoFocus sx={{ background: "#00B700", color: "white", "&:hover": {background: "transparent", color: "#00B700", border: "2px solid #00B700"}}}>
+        <DialogActions className="bg-black-200 flex justify-center items-center m-auto w-[100%]">
+          <Button onClick={handleClose} variant="outlined" autoFocus sx={{ background: "#00B700", color: "white", "&:hover": {background: "transparent", color: "#00B700", border: "2px solid #00B700"}, margin: "0 auto"}}>
             Ok
           </Button>
         </DialogActions>
--- src/components/dialogs/SuccessfulLoginDialog.jsx
+++ src/components/dialogs/SuccessfulLoginDialog.jsx
@@ -16,17 +16,17 @@ function SuccessfulLoginDialog({open, handleClose, user}) {
         aria-describedby="Welcome to WhatsAppGpt"
         user={user}
       >
-        <DialogTitle className="flex flex-col justify-between py-10 px-10 bg-primary">
+        <DialogTitle className="items-center bg-black-200 text-secondary border-b-2 border-secondary">
           {`Welcome to WhatsAppGpt, ${user}!`}
         </DialogTitle>
-        <DialogContent>
-            <VerifiedIcon className="secondary"/>
-          <DialogContentText className="text-[20px] text-secondary">
-            You have successfully signed up in!
-          </DialogContentText>
+        <DialogContent className="bg-black-200 text-secondary border-b-2 border-secondary" >
+          <div className="flex flex-col justify-around items-center text-secondary py-10 sm:gap-2 md:gap-10 ">
+            <VerifiedIcon fontSize="large" />
+            <p>You have successfully signed up in!</p>
+         </div>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} autoFocus>
+        <DialogActions className="bg-black-200 flex justify-center items-center m-auto w-[100%]">
+          <Button onClick={handleClose} variant="outlined" autoFocus sx={{ background: "#00B700", color: "white", "&:hover": {background: "transparent", color: "#00B700", border: "2px solid #00B700"}, margin: "0 auto"}}>
             Ok
           </Button>
         </DialogActions>
