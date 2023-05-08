3b20784f8a5e320d035f05096a4b4877e8359fc1
Dylan Petzer <dylan.petzer@entelect.co.za>
2023-05-08 13:28:51 +0200

    trying to get backend config to work

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,18 +1,25 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { getDatabase, ref, set } from "firebase/database";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyABqLy2LFmjUdNlxDPnfDJX3HBV8UE2Xgo",
-    authDomain: "whatsappgpt-f9e8d.firebaseapp.com",
-    projectId: "whatsappgpt-f9e8d",
-    storageBucket: "whatsappgpt-f9e8d.appspot.com",
-    messagingSenderId: "117700601010",
-    appId: "1:117700601010:web:59b636bc65ebc4291cd5ab",
-    measurementId: "G-WL07Q0M8NS"
-  };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyABqLy2LFmjUdNlxDPnfDJX3HBV8UE2Xgo",
+  authDomain: "whatsappgpt-f9e8d.firebaseapp.com",
+  projectId: "whatsappgpt-f9e8d",
+  storageBucket: "whatsappgpt-f9e8d.appspot.com",
+  messagingSenderId: "117700601010",
+  appId: "1:117700601010:web:903e8af8219176131cd5ab",
+  measurementId: "G-03QZRVX5W2"
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
@@ -27,12 +34,12 @@ export const signUpWithEmailAndPassword = async ({ name, lastName, email, phone,
     const user = userCredential.user;
 
     // Store the user data in the database
-    await set(ref(db, `users/${user.uid}`), {
-      Email_Address: email,
-      FirstName: name,
-      SecondName: lastName,
-      Phone_Number: phone,
-      Password: password,
+    await set(ref(db, `users/${phone}`), {
+      name,
+      lastName,
+      email,
+      phone,
+      password,
     });
 
     // Return the user object
