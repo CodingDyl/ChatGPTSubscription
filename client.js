27a5f6c99540c3ee924eee87b133e0d298119abb
Dylan Petzer <dylan.petzer@entelect.co.za>
2023-05-09 10:05:13 +0200

    backend semi connected

--- client.js
+++ client.js
@@ -1,40 +1,37 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { getDatabase, ref, set } from "firebase/database";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, addDoc, collection } from 'firebase/firestore';
 
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyABqLy2LFmjUdNlxDPnfDJX3HBV8UE2Xgo",
   authDomain: "whatsappgpt-f9e8d.firebaseapp.com",
+  databaseURL: "https://whatsappgpt-f9e8d-default-rtdb.firebaseio.com",
   projectId: "whatsappgpt-f9e8d",
   storageBucket: "whatsappgpt-f9e8d.appspot.com",
   messagingSenderId: "117700601010",
-  appId: "1:117700601010:web:903e8af8219176131cd5ab",
-  measurementId: "G-03QZRVX5W2"
+  appId: "1:117700601010:web:ced412d231b1c9c21cd5ab",
+  measurementId: "G-6DSB17B6VQ"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 const auth = getAuth(app);
-const db = getDatabase(app);
+const db = getFirestore(app);
 
-// Function to create a new user account and store user data in the database
 export const signUpWithEmailAndPassword = async ({ name, lastName, email, phone, password }) => {
   try {
-    // Create the user account
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     const user = userCredential.user;
 
-    // Store the user data in the database
-    await set(ref(db, `users/${phone}`), {
+    await addDoc(collection(db, `Users/${phone}`), {
       name,
       lastName,
       email,
@@ -42,12 +39,22 @@ export const signUpWithEmailAndPassword = async ({ name, lastName, email, phone,
       password,
     });
 
-    // Return the user object
     return user;
   } catch (error) {
-    // Handle errors
     console.error(error);
     throw error;
   }
+}
+
+export const signIn = async ({email, password}) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+
+      return user;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
 }
 
--- src/components/modal/LoginModal.jsx
+++ src/components/modal/LoginModal.jsx
@@ -16,7 +16,7 @@ import {
 } from "@mantine/core";
 import { GoogleButton, TwitterButton } from "../SocialButtons/SocialButtons";
 
-import { signUpWithEmailAndPassword } from "../../lib/firebase";
+import { signIn, signUpWithEmailAndPassword } from "../../../client.js";
 
 import SuccessfulLoginDialog from "../dialogs/SuccessfulLoginDialog";
 import FailedLoginDialog from "../dialogs/FailedLoginDialog";
@@ -45,160 +45,189 @@ function LoginModal({ opened, close }) {
           : null,
     },
   });
+  
   return (
     <>
-    <Modal
-      opened={opened}
-      onClose={close}
-      title="Authentication"
-      styles={{
-        body: { backgroundColor: "#090325" },
-        header: { backgroundColor: "#090325", color: "#00B700" },
-        close: {
-          color: "#00B700",
-          "&:hover": { background: "#00B700", color: "#090325" },
-        },
-        overlay: { backdropFilter: "blur(5px)" },
-      }}
-    >
-      <Paper radius="md" p="xl" withBorder>
-        <Text size="lg" weight={500} mb={15} fw={700}>
-          Welcome to WhatsAppGpt
-        </Text>
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="Authentication"
+        styles={{
+          body: { backgroundColor: "#090325" },
+          header: { backgroundColor: "#090325", color: "#00B700" },
+          close: {
+            color: "#00B700",
+            "&:hover": { background: "#00B700", color: "#090325" },
+          },
+          overlay: { backdropFilter: "blur(5px)" },
+        }}
+      >
+        <Paper radius="md" p="xl" withBorder>
+          <Text size="lg" weight={500} mb={15} fw={700}>
+            Welcome to WhatsAppGpt
+          </Text>
 
-        <form
-          onSubmit={(event) => {
-            event.preventDefault(); // prevent default form submission behavior
-            const { name, lastName, email, phone, password } = form.values;
-            if (type === "register") {
-              signUpWithEmailAndPassword({email, password, name, lastName, phone})
-                .then(() => {
-                  // handle successful user creation
-                  setOpenSuccess(true);
-                  setUser(`${name} ${lastName}`);
+          <form
+            onSubmit={(event) => {
+              event.preventDefault(); // prevent default form submission behavior
+              const { name, lastName, email, phone, password } = form.values;
+              if (type === "register") {
+                signUpWithEmailAndPassword({
+                  name,
+                  lastName,
+                  email,
+                  phone,
+                  password,
                 })
-                .catch((error) => {
-                  setOpenFailed(true);
-                  setMessage("There seems to be an issue with our servers, please try again later!");// handle error
-                });
-            } else {
-              setOpenFailed(true);
-              setMessage("There seems to be an issue with our servers, please try again later!");// handle login form submission
-            }
-          }}
-        >
-          <Stack>
-            {type === "register" && (
-              <>
-                <div className="flex justify-around gap-2">
+                  .then(() => {
+                    // handle successful user creation
+                    setOpenSuccess(true);
+                    setUser(`${name} ${lastName}`);
+                    close();
+                  })
+                  .catch((error) => {
+                    setOpenFailed(true);
+                    setMessage(error); // handle error
+                  });
+              } else {
+                signIn({ email, password })
+                  .then(() => {
+                    setOpenSuccess(true);
+                    setUser(`${name} ${lastName}`);
+                    close();
+                  })
+                  .catch((error) => {
+                    setOpenFailed(true);
+                    setMessage(
+                      "Please Ensure you have entered the correct email and password!"
+                    );
+                  }); // handle login form submission
+              }
+            }}
+          >
+            <Stack>
+              {type === "register" && (
+                <>
+                  <div className="flex justify-around gap-2">
+                    <TextInput
+                      label="Name"
+                      placeholder="Your name"
+                      value={form.values.name}
+                      onChange={(event) =>
+                        form.setFieldValue("name", event.currentTarget.value)
+                      }
+                      radius="md"
+                    />
+
+                    <TextInput
+                      label="Surname"
+                      placeholder="Your last name"
+                      value={form.values.lastName}
+                      onChange={(event) =>
+                        form.setFieldValue(
+                          "lastName",
+                          event.currentTarget.value
+                        )
+                      }
+                      radius="md"
+                    />
+                  </div>
+
                   <TextInput
-                    label="Name"
-                    placeholder="Your name"
-                    value={form.values.name}
+                    required
+                    label="Phone"
+                    placeholder="Your phone number"
+                    value={form.values.phone}
                     onChange={(event) =>
-                      form.setFieldValue("name", event.currentTarget.value)
+                      form.setFieldValue("phone", event.currentTarget.value)
                     }
                     radius="md"
                   />
+                </>
+              )}
 
-                  <TextInput
-                    label="Surname"
-                    placeholder="Your last name"
-                    value={form.values.lastName}
-                    onChange={(event) =>
-                      form.setFieldValue("lastName", event.currentTarget.value)
-                    }
-                    radius="md"
-                  />
-                </div>
-
-                <TextInput
-                  required
-                  label="Email"
-                  placeholder="hello@chat.gpt"
-                  value={form.values.email}
-                  onChange={(event) =>
-                    form.setFieldValue("email", event.currentTarget.value)
-                  }
-                  error={form.errors.email && "Invalid email"}
-                  radius="md"
-                />
-              </>
-            )}
-
-            <TextInput
-              required
-              label="Phone"
-              placeholder="Your phone number"
-              value={form.values.phone}
-              onChange={(event) =>
-                form.setFieldValue("phone", event.currentTarget.value)
-              }
-              radius="md"
-            />
-
-            <PasswordInput
-              required
-              label="Password"
-              placeholder="Your password"
-              value={form.values.password}
-              onChange={(event) =>
-                form.setFieldValue("password", event.currentTarget.value)
-              }
-              error={
-                form.errors.password &&
-                "Password should include at least 6 characters"
-              }
-              radius="md"
-            />
-
-            {type === "register" && (
-              <Checkbox
-                label="I accept terms and conditions"
-                checked={form.values.terms}
+              <TextInput
+                required
+                label="Email"
+                placeholder="hello@chat.gpt"
+                value={form.values.email}
                 onChange={(event) =>
-                  form.setFieldValue("terms", event.currentTarget.checked)
+                  form.setFieldValue("email", event.currentTarget.value)
                 }
-                styles={{
-                  icon: { color: "white" },
-                  input: {
-                    background: form.values.terms
-                      ? "#00B700 !important"
-                      : "white",
-                    border: form.values.terms
-                      ? "transparent !important"
-                      : "1px solid black",
-                  },
-                }}
+                error={form.errors.email && "Invalid email"}
+                radius="md"
               />
-            )}
-          </Stack>
 
-          <Group position="apart" mt="xl">
-            <Anchor
-              component="button"
-              type="button"
-              color="dimmed"
-              onClick={() => toggle()}
-              size="xs"
-            >
-              {type === "register"
-                ? "Already have an account? Login"
-                : "Don't have an account? Register"}
-            </Anchor>
-            <Button
-              type="submit"
-              radius="xl"
-              sx={{ backgroundColor: "#090325 !important", color: "#00B700" }}
-            >
-              {upperFirst(type)}
-            </Button>
-          </Group>
-        </form>
-      </Paper>
-    </Modal>
-    <SuccessfulLoginDialog open={openSuccess} handleClose={() => setOpenSuccess(false)} user={user} />
-    <FailedLoginDialog open={openFailed} handleClose={() => setOpenFailed(false)} message={message} />
+              <PasswordInput
+                required
+                label="Password"
+                placeholder="Your password"
+                value={form.values.password}
+                onChange={(event) =>
+                  form.setFieldValue("password", event.currentTarget.value)
+                }
+                error={
+                  form.errors.password &&
+                  "Password should include at least 6 characters"
+                }
+                radius="md"
+              />
+
+              {type === "register" && (
+                <Checkbox
+                  label="I accept terms and conditions"
+                  checked={form.values.terms}
+                  onChange={(event) =>
+                    form.setFieldValue("terms", event.currentTarget.checked)
+                  }
+                  styles={{
+                    icon: { color: "white" },
+                    input: {
+                      background: form.values.terms
+                        ? "#00B700 !important"
+                        : "white",
+                      border: form.values.terms
+                        ? "transparent !important"
+                        : "1px solid black",
+                    },
+                  }}
+                />
+              )}
+            </Stack>
+
+            <Group position="apart" mt="xl">
+              <Anchor
+                component="button"
+                type="button"
+                color="dimmed"
+                onClick={() => toggle()}
+                size="xs"
+              >
+                {type === "register"
+                  ? "Already have an account? Login"
+                  : "Don't have an account? Register"}
+              </Anchor>
+              <Button
+                type="submit"
+                radius="xl"
+                sx={{ backgroundColor: "#090325 !important", color: "#00B700" }}
+              >
+                {upperFirst(type)}
+              </Button>
+            </Group>
+          </form>
+        </Paper>
+      </Modal>
+      <SuccessfulLoginDialog
+        open={openSuccess}
+        handleClose={() => setOpenSuccess(false)}
+        user={user}
+      />
+      <FailedLoginDialog
+        open={openFailed}
+        handleClose={() => setOpenFailed(false)}
+        message={message}
+      />
     </>
   );
 }
